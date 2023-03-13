54db5a392b2a8798387cb1071c9b669f87cd257d
irfankhanktk <irfan.khan93093@gmail.com>
2023-03-13 10:38:27 +0500

    changes

--- src/services/api/api-urls.js
+++ src/services/api/api-urls.js
@@ -1,36 +1,36 @@
 export const IP = 'http://124.29.208.60:5159';
 export const URLS = {
-  base_url: `${IP}/api/`,
-  image_url: `${IP}/`,
-  auth: {
-    signup: 'doctor/signup',
-    login: 'doctor/login',
-    update_password: 'doctor/updatePassword',
-    change_password: 'doctor/changePassword',
-    otp_verify: 'doctor/otpVerify',
-    forget_password: 'doctor/forgetPassword',
-    // update_profile: 'user/updateProfile',
-  },
-  availability: {
-    add: 'doctor/addDoctorAvailability',
-    getDoctorHospitals: 'doctor/getDoctorHospital',
-    getDoctorHospitalDetails: 'doctor/getDoctorHospitalDetail',
-    edithospitalAvailabilityDetails: 'doctor/editHospitalAvailability',
-    delete: 'doctor/deleteDoctorAvailability', //need availability_id
-    change: 'doctor/appointmentStatus',
-  },
-  all_hospitals: 'doctor/allHospital',
-  appointment: {
-    list: 'doctor/listAppointments',
-    details: 'doctor/appointmentDetail',
-    home_counter: 'doctor/counterAppointments',
-    status_change: 'doctor/appointmentStatus',
-    complete_appoinment: 'doctor/doctorCompleteAppointment',
-  },
-  categories: {
-    getAll: 'doctor/allSpecialization',
-  },
-  notification: {
-    get_notification: 'doctor/getNotification',
-  },
+    base_url: `${IP}/api/`,
+    image_url: `${IP}/`,
+    auth: {
+        signup: 'doctor/signup',
+        login: 'doctor/login',
+        update_password: 'doctor/updatePassword',
+        change_password: 'doctor/changePassword',
+        otp_verify: 'doctor/otpVerify',
+        forget_password: 'doctor/forgetPassword',
+        update_profile: 'user/updateProfile',
+    },
+    availability: {
+        add: 'doctor/addDoctorAvailability',
+        getDoctorHospitals: 'doctor/getDoctorHospital',
+        getDoctorHospitalDetails: 'doctor/getDoctorHospitalDetail',
+        edithospitalAvailabilityDetails: 'doctor/editHospitalAvailability',
+        delete: 'doctor/deleteDoctorAvailability', //need availability_id
+        change: 'doctor/appointmentStatus',
+    },
+    all_hospitals: 'doctor/allHospital',
+    appointment: {
+        list: 'doctor/listAppointments',
+        details: 'doctor/appointmentDetail',
+        home_counter: 'doctor/counterAppointments',
+        status_change: 'doctor/appointmentStatus',
+        complete_appoinment: 'doctor/doctorCompleteAppointment',
+    },
+    categories: {
+        getAll: 'doctor/allSpecialization',
+    },
+    notification: {
+        get_notification: 'doctor/getNotification',
+    },
 };
