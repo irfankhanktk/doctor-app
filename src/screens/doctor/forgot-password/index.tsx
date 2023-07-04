b64efb37e9ce18bd76d5fdde2d0e5a3f182b3a9a
Mohsin-Khattak <mohsinkhattak095@gmail.com>
2023-07-04 11:51:30 +0500

    forgot screen input validation add

--- src/screens/doctor/forgot-password/index.tsx
+++ src/screens/doctor/forgot-password/index.tsx
@@ -1,23 +1,20 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RenewLogo } from 'assets/doctor/icons';
 import { auth_bg } from 'assets/doctor/images';
-import OtpModal from 'components/molecules/doctor/modals/otp-modal';
+import { PrimaryButton } from 'components/atoms/buttons';
+import PrimaryInput from 'components/atoms/inputs';
+import { KeyboardAvoidScrollview } from 'components/atoms/keyboard-avoid-scrollview';
 import OtpModalRenewPassword from 'components/molecules/doctor/modals/otp-modal-signup-renewpassword.js';
-import { colors } from 'config/colors';
-import { height, mvs, width } from 'config/metrices';
+import { height, width } from 'config/metrices';
 import { useFormik } from 'formik';
+import { useAppDispatch } from 'hooks/use-store';
 import React from 'react';
 import { ImageBackground, View } from 'react-native';
-import { onForgot, onSignup } from 'services/api/doctor/api-actions';
+import { onForgot } from 'services/api/doctor/api-actions';
 import i18n from 'translation';
 import Bold from 'typography/bold-text';
-import { forgotemailFormValidation, signupFormValidation } from 'validations';
-import { PrimaryButton } from 'components/atoms/buttons';
-import PrimaryInput, { PrimaryPhoneInput } from 'components/atoms/inputs';
-import { KeyboardAvoidScrollview } from 'components/atoms/keyboard-avoid-scrollview';
-import { useAppDispatch } from 'hooks/use-store';
+import { forgotemailFormValidation } from 'validations';
 import RootStackParamList from '../../types/navigation-types/root-stack';
-import Medium from 'typography/medium-text';
 import styles from './styles';
 type props = NativeStackScreenProps<RootStackParamList, 'ForgotPassword'>;
 
@@ -26,14 +23,9 @@ const ForgotPassword = (props: props) => {
   const { t } = i18n;
   const dispatch = useAppDispatch();
   const initialValues = {
-    // first_name: '',
-    // last_name: '',
+ 
     email: '',
-    // phone: '',
-    // birthday: '2000-01-31',
-
-    // confirm_password: '',
-    // password: '',
+   
   };
   const [loading, setLoading] = React.useState(false);
   // const [otpModal, setOtpModal] = React.useState(false);
@@ -51,15 +43,26 @@ const ForgotPassword = (props: props) => {
 
   const onSubmit = async () => {
     try {
-      setLoading(true);
-      const res = await onForgot(values);
-      setLoading(false);
+      if (isValid && Object.keys(touched).length > 0) {
+      try {
+        
+        setLoading(true);
+        const res = await onForgot(values);
+        console.log('res===>>>>> forgot', res);
+        setOtpModalVisible(true);
+      } catch (error) {
+      console.log('error=>', error);
+        
+      }
+      }else{
+        setFieldTouched('email', true);
 
-      console.log('res===>>>>> forgot', res);
-      setOtpModalVisible(true);
+      }
     } catch (error) {
       console.log('error=>', error);
-      setLoading(false);
+      
+    }finally{
+      setLoading(false)
     }
   };
   return (
